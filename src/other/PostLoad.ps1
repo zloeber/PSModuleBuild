c1f6237acea8192b1a198ad4c31d7bcf2600bc3e
Zachary Loeber <zloeber@gmail.com>
2016-07-30 13:39:24 -0500

    Fixed up pre and post load template files with some helpful variables and methods

--- src/other/PostLoad.ps1
+++ src/other/PostLoad.ps1
@@ -1,28 +1,35 @@
 
-<#
- Created on:   6/25/2015 10:01 AM
- Created by:   Zachary Loeber
- Module Name:  NLogModule
- Requires: http://nlog-project.org/
-#>
+# Use this variable for any path-sepecific actions (like loading dlls and such) to ensure it will work in testing and after being built
+$MyModulePath = $(
+    Function Get-ScriptPath {
+        $Invocation = (Get-Variable MyInvocation -Scope 1).Value
+        if($Invocation.PSScriptRoot) {
+            $Invocation.PSScriptRoot
+        }
+        Elseif($Invocation.MyCommand.Path) {
+            Split-Path $Invocation.MyCommand.Path
+        }
+        elseif ($Invocation.InvocationName.Length -eq 0) {
+            (Get-Location).Path
+        }
+        else {
+            $Invocation.InvocationName.Substring(0,$Invocation.InvocationName.LastIndexOf("\"));
+        }
+    }
 
-Export-ModuleMember -Variable NLogConfig
-#endregion Methods
+    Get-ScriptPath
+)
 
-$Logger = $null
-$NLogConfig = Get-NewLogConfig
 
 #region Module Cleanup
 $ExecutionContext.SessionState.Module.OnRemove = {
-    if ( Get-NLogDllLoadState ) {
-        try {
-            get-module | where {($_.Name -eq 'nlog') -or ($_.Name -eq 'Nlog45')} | foreach {
-                Remove-Module $_
-            }
-        }
-        catch { 
-            Write-Warning "Unable to uninitialize module."
-        }
-    }    
+    # Action to take if the module is removed
+}
+
+$null = Register-EngineEvent -SourceIdentifier ( [System.Management.Automation.PsEngineEvent]::Exiting ) -Action {
+    # Action to take if the whole pssession is killed
 }
 #endregion Module Cleanup
+
+# Exported members
+#Export-ModuleMember -Variable SomeVariable -Function  *
